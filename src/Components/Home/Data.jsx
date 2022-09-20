21c4eb9a4f0d284f14af509e15aed06fd4a30aef
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-09-20 13:27:37 +0530

    about page added

--- src/Components/Home/Data.jsx
+++ src/Components/Home/Data.jsx
@@ -59,7 +59,7 @@ const Data = () => {
       <p className="home_description">
       I'm creative designer based in India, and I'm very passionate and dedicated to my work.
       </p>
-      <a href="#contact" className="button buttom-flex">
+      <a href="#contact" className="button button--flex">
         Say Hello
         <svg
           class="button__icon"
--- src/Components/Home/Social.jsx
+++ src/Components/Home/Social.jsx
@@ -3,14 +3,25 @@ import React from "react";
 const Social = () => {
   return (
     <div className="home_social">
-      <a href="" className="home_social_icon" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/the-piyush-agrawal/"
+        className="home_social_icon"
+        target="_blank"
+      >
         <i className="uil uil-linkedin-alt"></i>
-        
       </a>
-      <a href="" className="home_social_icon" target="_blank">
+      <a
+        href="https://www.instagram.com/joy_agrawal_/"
+        className="home_social_icon"
+        target="_blank"
+      >
         <i className="uil uil-instagram"></i>
       </a>
-      <a href="" className="home_social_icon" target="_blank">
+      <a
+        href="https://github.com/piyush-agrawal6"
+        className="home_social_icon"
+        target="_blank"
+      >
         <i className="uil uil-github"></i>
       </a>
     </div>
