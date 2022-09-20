697166cfa30d3685b3dc4532986af5eaebd5b750
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-09-20 11:53:10 +0530

    home added

---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a href="" className="home_social_icon" target="_blank">
+        <i className="uil uil-linkedin-alt"></i>
+        
+      </a>
+      <a href="" className="home_social_icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="" className="home_social_icon" target="_blank">
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
